9092c777896e5fab9fec5abebb1303a8c6443f82
SimHongSub <ghdtjq2038@gmail.com>
2021-02-16 10:47:16 +0900

    노래 화면, 사용자 화면 컴포넌트 추가

---
+++ src/components/SingScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingScreen = () => {
+  return <div>노래 화면</div>;
+};
+
+export default SingScreen;
---
+++ src/components/UserScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserScreen = () => {
+  return <div>사용자 화면</div>;
+};
+
+export default UserScreen;
