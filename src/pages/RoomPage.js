bd34dbb87cb059c92154227c019797753670a175
SimHongSub <ghdtjq2038@gmail.com>
2021-02-16 11:07:39 +0900

    bootstrap layout 적용

--- src/pages/RoomPage.js
+++ src/pages/RoomPage.js
@@ -1,18 +1,21 @@
 import React from "react";
 import SingScreen from "../components/SingScreen";
 import UserScreen from "../components/UserScreen";
+import { Col, Container, Row } from "react-bootstrap";
 
 const RoomPage = () => {
   return (
-    <div>
-      <div>
-        <SingScreen />
-      </div>
-      <div>
-        <UserScreen />
-        <UserScreen />
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <SingScreen />
+        </Col>
+        <Col>
+          <UserScreen />
+          <UserScreen />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
